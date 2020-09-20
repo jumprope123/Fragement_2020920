8027ec0451ff90ab34ca3414c56b43186594a271
JiHwan Shin <jumprope123@naver.com>
2020-09-20 16:20:16 +0900

    프레그먼트kt + xml 기초 작성

---
+++ app/src/main/java/kr/co/tjoeun/fragement_2020920/Fragements/FirstFragment.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.fragement_2020920.Fragements
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.tjoeun.fragement_2020920.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragement_first, container, false)
+    }
+
+}

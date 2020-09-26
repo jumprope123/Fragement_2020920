a919b2c61b0a69afadbaba11bc52333615d02ac7
JiHwan Shin <jumprope123@naver.com>
2020-09-26 10:33:47 +0900

    응용문제 답안 Toast띄우기 context변수를 활용

--- app/src/main/java/kr/co/tjoeun/fragement_2020920/Fragements/SecondFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragement_2020920/Fragements/SecondFragment.kt
@@ -4,31 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragement_first.*
 import kotlinx.android.synthetic.main.fragement_second.*
 import kr.co.tjoeun.fragement_2020920.R
 
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment(){
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragement_first, container, false)
+        return inflater.inflate(R.layout.fragement_second,container,false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         toastBtn.setOnClickListener {
-
-            Toast.makeText(context,"버튼이 눌림",Toast.LENGTH_SHORT).show()
-
+            secondTxt.text = "버튼이 눌림"
         }
-
     }
-
 }
